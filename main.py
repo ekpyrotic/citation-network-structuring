ba14dde5f3ff62e31c07bb93cb2092cae7856dbe
Valentine W. <valentinewilson@192.168.1.165>
2017-01-03 00:45:35 -0500

    configured beautiful soup and requests

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from bs4 import BeautifulSoup
+import requests as rq
 #testing different webscrapers:
-#/using beautiful soup
+#using beautiful soup
+r = rq.get("http://genomebiology.biomedcentral.com/articles/10.1186/gb-2009-10-3-r25")
+soup = BeautifulSoup(r.text,"html.parser")
+print(soup.title)
