ec513f7688b218bb56c5bf0e4bc7a48f8d029b73
Valentine W. <valentinewilson@192.168.1.165>
2017-01-03 00:01:38 -0500

    lmao

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
-
-//testing different webscrapers:
-//here is an implementation of Christian Kreibich's project scholar.py
+from bs4 import BeautifulSoup
+#testing different webscrapers:
+#/using beautiful soup
