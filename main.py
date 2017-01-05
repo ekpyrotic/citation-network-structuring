a42b7e63923242991825d0730321c4f6984d1eb8
Valentine W. <valentinewilson@192.168.1.165>
2017-01-05 17:23:16 -0500

    - found a usable web crawer, adjusted it to fit our domain - its finds the citations for each article we select - next step is to find links between articles in our corpus

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
-from bs4 import BeautifulSoup,SoupStrainer
-import requests 
-import scrapy 
-import re
+import scholar
 
-#testing different webscrapers:
-#using beautiful soup
-r = requests.get("http://genomebiology.biomedcentral.com/articles/10.1186/gb-2009-10-3-r25")
-soup = BeautifulSoup(r.text,"html.parser")
-print(soup.title)
+#using Christian Kreibich google scholar parser called scholar.py, ty Christian!
+def setup(citationformat, keywords,maxpage):
+    querier = scholar.ScholarQuerier()
+    settings = scholar.ScholarSettings()
+    query = scholar.SearchScholarQuery()
+    settings.set_citation_format(citationformat)
+    querier.apply_settings(settings)
+    query.set_words_some(keywords)
+    query.set_num_page_results(maxpage)
+    querier.send_query(query)
+    results = open('results', 'w')
+    for art in querier.articles:
+        results.write(art.attrs["title"][0]+"\n")
+    results.close()
+    return querier.articles
+    
+def build_comparison_2d_array(list_names):
+    map = [[0 for x in range(len(list_names))] for y in range(len(list_names))] 
+    return map
+def build_citation_map(list_articles):
+    citationmap = {}
+    for art in list_articles:
+        citationmap[art.attrs["title"][0]] = art.attrs["url_citations"][0]
+        print(art.attrs["title"][0] + " : " + art.attrs["url_citations"][0])
+def print_2d_array(map):
+    for row in map:
+        for item in row:
+            print(item,end="")
+        print("\n")
+def print_map(map):
+    for key in map.keys():
+        print(key +" : " + map[key])
 
-# testing a scrapy webscrawler base class
+articles = setup(scholar.ScholarSettings.CITFORM_BIBTEX, "physics",10)
+map = build_citation_map(articles)
+#print_map(map)
 
-def cool_spider(max_pages):
-    page = 1
-    while page <= max_pages:
-        url = 'http://genomebiology.biomedcentral.com/articles/10.1186/gb-2009-10-3-r25' + str(page)
-        source_code = requests.get(url)
-        plain_text = source_code.text
-        Soup = BeautifulSoup(plain_text)
-      
-        for link in Soup.findAll('a', {'class': ''}):
-            href = link.get('href')
-            print(href)
-        page += 1
 
-cool_spider(5)
+
